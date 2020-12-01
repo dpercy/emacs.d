5af4a417efe1d7e9bae36745f4b103e7c6b7e569
David Percy <davetp425@gmail.com>
2020-12-01 02:48:21 -0500

    more config

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+(progn ; reduce filesystem clutter
+  (setq make-backup-files nil)
+  (setq auto-save-default nil)
+  )
 (progn ; custom.el
   (setq custom-file "~/.emacs.d/custom.el")
   (ignore-errors (load custom-file))
@@ -12,30 +16,49 @@
   (unless (package-installed-p 'use-package)
     (package-install 'use-package))
   (require 'use-package)
+  (setq use-package-always-ensure t)
+  ;; TODO consider use-package-always-defer
+  (setq use-package-compute-statistics t)
+  ;; TODO try borg?
+  ;; - submodules (more declarative, easier to contribute)
+  ;; - simpler than straight.el?
+  ;; - annoying terminology :(
+  ;; - no dependency resolution...
+  ;; TODO try straight.el?
+  ;; - handles installation more declaratively?
+  ;; - easier to edit / contribute to packages?
+  ;; https://github.com/raxod502/straight.el
+  (setq use-package-verbose t)
   )
-(progn ; reduce filesystem clutter
-  (setq make-backup-files nil)
-  (setq auto-save-default nil)
+(progn ; no gui
+  (menu-bar-mode 0)
+  (tool-bar-mode 0)
+  (scroll-bar-mode 0)
+  (setq inhibit-startup-screen t)
   )
-(save-place-mode 1) ; remember position in file
+(progn ; ui behavior
+  (defalias 'yes-or-no-p 'y-or-n-p)
+  (save-place-mode 1)
+  (setq help-window-select t)
+  ;; TODO help/compile/magit in-place? no split/popup
+  )
 (use-package helpful
-  :config
-  (progn
-    ;; :config runs after 'helpful loads, so if helpful
-    ;; doesn't load (maybe it failed to install) then
-    ;; we still have the default help bindings.
-    
-    ;; The built-in describe-function also covers macros,
-    ;; so it corresponds to helpful-callable.
-    ;; TODO would be nice if these remappings were provided as
-    ;; a global minor mode ("global-helpful-mode"?)
-    (global-set-key [remap describe-function] 'helpful-callable)
-    (global-set-key [remap describe-key]      'helpful-key)
-    (global-set-key [remap describe-symbol]   'helpful-symbol)
-    (global-set-key [remap describe-variable] 'helpful-variable)
-    ;; TODO C-h m ?
-    )
-  )
+  :bind (
+	 ;; The built-in describe-function also covers macros,
+	 ;; so it corresponds to helpful-callable.
+	 ;; TODO would be nice if these remappings were provided as
+	 ;; a global minor mode ("global-helpful-mode"?)
+	 ([remap describe-function] . helpful-callable)
+	 ([remap describe-key]      . helpful-key)
+	 ([remap describe-symbol]   . helpful-symbol)
+	 ([remap describe-variable] . helpful-variable)
+	 ;; TODO C-h m ?
+	 ))
+(use-package hideshow-org
+  :config (add-hook 'emacs-lisp-mode-hook 'hs-org/minor-mode))
+(use-package paredit
+  :config (add-hook 'emacs-lisp-mode-hook 'paredit-mode))
+(use-package magit)
 
 ;; What various things do I want?
 ;; - UI tweaks (colors, toolbar, menu bar, initial buffer)
@@ -45,20 +68,10 @@
 ;;   - spaces by default, all languages
 ;;   - manual (use tab/shifttab)
 
-;; - remember some stuff across restarts?
-;;   - position in file
-
-;; - elisp:
-;;    - fold blocks (hideshow?)
-;;    - paredit
+;; - finding files in git
 
 ;; - C++ IDE:
 ;;    - indentation
 ;;    - completion
 ;;    - format on save
 ;;    - compile
-
-
-
-;; TODO things I might want to try:
-;; - 'helpful' - nicer 'help' pages, especially for coding elisp
