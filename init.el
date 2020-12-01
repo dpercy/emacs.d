6ab09e054136b62d1b77565804986d0fabc3c6c0
David Percy <davetp425@gmail.com>
2020-12-01 03:42:37 -0500

    more packages and tweaks

--- init.el
+++ init.el
@@ -16,6 +16,10 @@
   (unless (package-installed-p 'use-package)
     (package-install 'use-package))
   (require 'use-package)
+
+  ;; always-ensure makes the first startup slow, and may make the first startup
+  ;; fail if some package changed. But leaving always-ensure false also has
+  ;; problems: if
   (setq use-package-always-ensure t)
   ;; TODO consider use-package-always-defer
   (setq use-package-compute-statistics t)
@@ -30,18 +34,39 @@
   ;; https://github.com/raxod502/straight.el
   (setq use-package-verbose t)
   )
-(progn ; no gui
+(progn ; gui appearance
   (menu-bar-mode 0)
   (tool-bar-mode 0)
   (scroll-bar-mode 0)
   (setq inhibit-startup-screen t)
+  ;; TODO color theme
   )
 (progn ; ui behavior
   (defalias 'yes-or-no-p 'y-or-n-p)
   (save-place-mode 1)
   (setq help-window-select t)
-  ;; TODO help/compile/magit in-place? no split/popup
+
+  ;; When displaying a buffer (as in compile, help, git), I generally
+  ;; want to use the 'same' window to avoid creating splits. The
+  ;; exception is: if the buffer is already open in some window, I
+  ;; want to 'reuse' it to avoid having the same buffer open twice.
+  (setq display-buffer-base-action '((display-buffer-reuse-window
+				      display-buffer-same-window
+				      )))
+
+  ;; Use paths to disambiguate buffer names.
+  ;; (As opposed to the funky default angle-bracket notation.)
+  (setq uniquify-buffer-name-style 'forward)
+  (setq uniquify-strip-common-suffix nil)
+
+  ;; TODO better window navigation (windmove?)
   )
+(progn ; editing behavior
+  (setq-default fill-column 80)
+  (setq-default indent-tabs-mode nil)
+  ;; TODO less aggressive indentation? let me manually tab/untab?
+  )
+;; general
 (use-package helpful
   :bind (
 	 ;; The built-in describe-function also covers macros,
@@ -55,23 +80,25 @@
 	 ;; TODO C-h m ?
 	 ))
 (use-package hideshow-org
-  :config (add-hook 'emacs-lisp-mode-hook 'hs-org/minor-mode))
+  :hook ((emacs-lisp-mode . hs-hide-all)
+	 (emacs-lisp-mode . hs-org/minor-mode)))
+(use-package magit
+  :defer t)
+(use-package paren
+  :hook (prog-mode . show-paren-mode)
+  )
+(use-package git-gutter
+  :hook prog-mode)
+; TODO git file navigation
+(use-package undo-tree
+  :init (global-undo-tree-mode))
+;; lisp
 (use-package paredit
-  :config (add-hook 'emacs-lisp-mode-hook 'paredit-mode))
-(use-package magit)
-
-;; What various things do I want?
-;; - UI tweaks (colors, toolbar, menu bar, initial buffer)
-;; - better window movement?? C-x C-a C-aa C-aaah
-;; - git diff in margin
-;; - indentation:
-;;   - spaces by default, all languages
-;;   - manual (use tab/shifttab)
-
-;; - finding files in git
-
-;; - C++ IDE:
+  :hook (emacs-lisp-mode . paredit-mode))
+;; TODO C++
 ;;    - indentation
 ;;    - completion
 ;;    - format on save
 ;;    - compile
+
+;; TODO js
